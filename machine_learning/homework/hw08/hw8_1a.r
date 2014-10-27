056573166b9e4ab7defc7e0fca7b13875b71cf49
Elijah Bernstein-Cooper <ezbc@astro.wisc.edu>
2014-10-27 07:22:40 -0500

    Completed machine learning hw11

---
+++ machine_learning/homework/hw08/hw8_1a.r
@@ -0,0 +1,36 @@
+
+x <- seq(1, 100)
+y <- dnorm(x, m=10, sd=5) + 10*dnorm(x, m=90, sd=40) + 5*dnorm(x, m=33, sd=20)
+
+y_noisey <- y + rnorm(100, m=0, sd=0.001)
+y_noisey <- y_noisey / max(y_noisey)
+
+library(pspline)
+
+fit <- smooth.Pspline(x, y_noisey, method=3)
+plot(x, y_noisey, xlab="x", ylab="y")
+lines(fit$x, fit$ysmth)
+
+R <- 1/length(x) * sum((y_noisey - fit$ysmth)^2)
+
+# Now find optimal lambda value
+lambdas = seq(0.1*fit$spar, 10*fit$spar, by=0.01*fit$spar)
+Rs = array(c(100))
+for (lambda in 1:length(lambdas)){
+  fit <- smooth.Pspline(x, y_noisey, method=1, spar=lambdas[lambda])
+  Rs[lambda] <- 1/length(x) * sum((y_noisey - fit$ysmth)^2)
+}
+R_optimal = min(Rs)
+
+fit <- smooth.Pspline(x, y_noisey, method=1, spar=lambdas[which.min(Rs)])
+
+# Plot
+png(paste(c("hw8_1a_f_vs_x.png"), collapse="", sep=""), height=500, width=500)
+plot(x, y_noisey, xlab="x", ylab="y")
+lines(fit$x, fit$ysmth)
+graphics.off()
+R
+R/R_optimal
+
+
+
